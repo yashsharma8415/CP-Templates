13d3691a2fc5885508708597c224167c13644cd4
Yash Sharma <yashsharma8415@gmail.com>
2021-07-10 00:54:50 +0530

    Create LCA.cpp

---
+++ LCA.cpp
@@ -0,0 +1,52 @@
+// Code for finding lca of two nodes.
+// Time Complexity - O(n * logn) for build and O(logn) for query where n is the number of nodes in tree.
+
+int l, timer;
+int tin[LIM], tout[LIM], up[LIM][22];
+
+void dfs(int v, int p)
+{
+    tin[v] = ++timer;
+    up[v][0] = p;
+
+    for (int i = 1; i <= l; ++i)
+        up[v][i] = up[up[v][i - 1]][i - 1];
+
+    for (int u : adj[v])
+    {
+        if (u != p)
+            dfs(u, v);
+    }
+
+    tout[v] = ++timer;
+}
+
+bool isAncestor(int u, int v)
+{
+    return tin[u] <= tin[v] && tout[u] >= tout[v];
+}
+
+int lca(int u, int v)
+{
+    if (isAncestor(u, v))
+        return u;
+
+    if (isAncestor(v, u))
+        return v;
+
+    for (int i = l; i >= 0; --i)
+    {
+        if (!isAncestor(up[u][i], v))
+            u = up[u][i];
+    }
+
+    return up[u][0];
+}
+
+void initLCA(int root)
+{
+    timer = 0;
+    l = ceil(log2(n));
+
+    dfs(root, root);
+}
