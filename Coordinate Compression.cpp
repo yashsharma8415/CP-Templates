3311fdb424d06653db6a26092ca803d6bab34736
Yash Sharma <yashsharma8415@gmail.com>
2021-08-03 15:47:12 +0530

    Create Coordinate Compression.cpp

---
+++ Coordinate Compression.cpp
@@ -0,0 +1,22 @@
+// Code for coordinate compression of a vector.
+// Time Complexity - O(n * logn) where n is the length of vector a.
+
+void coordinateCompression(vector<int>& a)
+{
+    int n = a.size();
+    vector<pair<int, int>> pairs(n);
+
+    for (int i = 0; i < n; ++i)
+        pairs[i] = {a[i], i};
+
+    sort(pairs.begin(), pairs.end());
+    int nxt = 0;
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (i > 0 && pairs[i - 1].first != pairs[i].first)
+            nxt++;
+
+        a[pairs[i].second] = nxt;
+    }
+}
