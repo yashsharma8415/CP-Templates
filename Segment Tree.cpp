f364092ef67c9ae768e768a10079f679214e6d5b
Yash Sharma <yashsharma8415@gmail.com>
2021-11-03 18:29:14 +0530

    Update Segment Tree.cpp

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -1,12 +1,18 @@
 // Code for segment tree data structure.
-// Time Complexity - O(n) for build and O(log n) for query where n is the length of array.
+// Time Complexity - O(n) for build and O(log n) for query and update where n is the length of array.
 
-int segmentTree[4 * LIM], a[LIM];
+int idElement;
+int st[4 * LIM], a[LIM];
+
+int combine(int a, int b)
+{
+    return a + b;
+}
 
 void build(int index, int l, int r)
 {
     if (l == r)
-        segmentTree[index] = a[l];
+        st[index] = a[l];
     else
     {
         int mid = (l + r) / 2;
@@ -14,29 +20,29 @@ void build(int index, int l, int r)
         build(2 * index, l, mid);
         build(2 * index + 1, mid + 1, r);
 
-        segmentTree[index] = segmentTree[index * 2] +
-                             segmentTree[index * 2 + 1];
+        st[index] = combine(st[index * 2],
+                            st[index * 2 + 1]);
     }
 }
 
 int query(int index, int curL, int curR, int queryL, int queryR)
 {
     if (queryR < curL || queryL > curR)
-        return 0;
+        return idElement;
 
     if (queryL <= curL && queryR >= curR)
-        return segmentTree[index];
+        return st[index];
 
     int mid = (curL + curR) / 2;
 
-    return query(2 * index, curL, mid, queryL, queryR) +
-           query(2 * index + 1, mid + 1, curR, queryL, queryR);
+    return combine(query(2 * index, curL, mid, queryL, queryR),
+                   query(2 * index + 1, mid + 1, curR, queryL, queryR));
 }
 
 void update(int index, int l, int r, int pos, int newVal)
 {
     if (l == r)
-        segmentTree[index] = newVal;
+        st[index] = newVal;
     else
     {
         int mid = (l + r) / 2;
@@ -46,7 +52,17 @@ void update(int index, int l, int r, int pos, int newVal)
         else
             update(2 * index + 1, mid + 1, r, pos, newVal);
 
-        segmentTree[index] = segmentTree[index * 2] +
-                             segmentTree[index * 2 + 1];
+        st[index] = combine(st[index * 2],
+                            st[index * 2 + 1]);
     }
+}
+
+void initSegmentTree(int n)
+{
+    for (int i = 0; i < n + 5; ++i)
+        a[i] = 0;
+
+    idElement = 0;
+
+    build(1, 0, n - 1);
 }
