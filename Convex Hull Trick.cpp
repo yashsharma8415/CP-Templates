5ac16d4b44c51d57956a3c69fdd867dfbad3a4b9
Yash Sharma <yashsharma8415@gmail.com>
2021-12-22 17:25:56 +0530

    Update Convex Hull Trick.cpp

--- Convex Hull Trick.cpp
+++ Convex Hull Trick.cpp
@@ -4,39 +4,57 @@
 struct line
 {
     int m, c;
- 
+
     int getValue(int x)
     {
-        return m*x + c;
+        return m * x + c;
     }
- 
+
     double intersectionX(line l)
     {
         return (c - l.c + 0.0) / (l.m - m);
     }
 };
- 
+
+deque<line> dq;
+
+int getIndex(int x)
+{
+    int l = 0, r = dq.size() - 1;
+
+    while (l < r)
+    {
+        int mid = (l + r) / 2;
+
+        if (x > dq[mid].intersectionX(dq[mid + 1]))
+            l = mid + 1;
+        else
+            r = mid;
+    }
+
+    return l;
+}
+
 void convexHullTrick()
 {
-    deque<line> dq;
- 
-    dq.pf(some initial line);
- 
-    for(int i=1; i<n; i++)
+    dq.push_front(some initial line);
+
+    for (int i = 0; i < n; i++)
     {
-        while(dq.size() >= 2 && dq.back().getValue(x[i]) >= dq[dq.size()-2].getValue(x[i]))
-            dq.popb();
- 
-        int val = dq.back().getValue(x[i]) + constant;
+        while (dq.size() >= 2 && dq.back().getValue(x[i]) >= dq[dq.size() - 2].getValue(x[i]))
+            dq.pop_back();
+
+        int index = getIndex(x[i]);
+        int val = dq[index].getValue(x[i]) + constant;
         ans = val;
- 
+
         line cur = {make line from this x[i]};
- 
-        while(dq.size() >= 2 && cur.intersectionX(dq[0]) <= dq[0].intersectionX(dq[1]))
-            dq.popf();
- 
-        dq.pf(cur);
+
+        while (dq.size() >= 2 && cur.intersectionX(dq[0]) <= dq[0].intersectionX(dq[1]))
+            dq.pop_front();
+
+        dq.pop_front(cur);
     }
- 
+
     return;
 }
